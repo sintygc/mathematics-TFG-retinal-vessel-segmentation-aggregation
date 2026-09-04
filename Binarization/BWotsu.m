6cd01d255024c892de08c1702b11d329bbf67129
sintygc <75248023+sintygc@users.noreply.github.com>
2026-09-04 12:14:36 +0200

    Delete Binarization directory

--- Binarization/BWotsu.m
+++
@@ -1,9 +0,0 @@
-function [result,T] = BWotsu(image,varargin)
-
-%image: 584x565 uint8 u=256 [0,255]
-% image=double(image);
-% image=scale_01(image); 
-T = graythresh(image); %=0.1059
-result = imbinarize(image,T);
-
-end
--- Binarization/BWotsuA.m
+++
@@ -1,7 +0,0 @@
-function [result,T] = BWotsuA(image,varargin)
-
-%image: 584x565 uint8 u=256 [0,255]
-result = imbinarize(image,'adaptive');
-T = 0;
-
-end
